135ffb56bb86660831015a30e9236791d8cfae13
JakeTodoroki <taco7897@gmail.com>
2022-12-07 22:56:01 -0700

    excersize 6 - attempt reset button

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // console.log('sanity check my boy')
 const inp = document.getElementById('input-id')
 const btn = document.getElementById('submit-button')
+const btnReset = document.getElementById('reset-button')
 const list = document.getElementById('todo-list')
+
+btn.addEventListener('click', function(evt){
+    const li = document.createElement('li')
+    const inp = document.querySelector('input')
+    li.textContent = inp.value
+    document.querySelector('ul').appendChild(li)
+    inp.value = ''
+    const btnReset = document.querySelector(li)
+})
